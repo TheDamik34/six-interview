422e5c88d075f400ba271dfdaeeac6ca193620c3
Damian Jacewicz <jacewiczdamian@gmail.com>
2025-08-06 16:50:47 +0200

    implementation shouldCreateNewMissionWithName

--- src/main/java/org/example/Mission.java
+++ src/main/java/org/example/Mission.java
@@ -3,10 +3,21 @@ package org.example;
 import java.util.Objects;
 
 public final class Mission {
+    private String name;
     private MissionStatus missionStatus;
 
     public Mission() {
+        this.name = "Unknown Mission";
         this.missionStatus = MissionStatus.SCHEDULED;
+    }
+
+    public Mission(String name) {
+        this.name = name;
+        this.missionStatus = MissionStatus.SCHEDULED;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public MissionStatus getMissionStatus() {
