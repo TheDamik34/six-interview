9fd09dcf782140cf1f79c00bb2a54c49a1ae1bab
Damian Jacewicz <jacewiczdamian@gmail.com>
2025-08-06 11:35:47 +0200

    requ4:test1

---
+++ src/test/java/org/example/MissionTests.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MissionTests {
+
+    @Test
+    void shouldCreateNewMission() {
+        // given + when
+        Mission mission = new Mission();
+
+        // then
+        assertEquals(MissionStatus.SCHEDULED, mission.getStatus());
+    }
+}
