d7d55fb560dca39f9719a1950a18decb34e12ecf
Damian Jacewicz <jacewiczdamian@gmail.com>
2025-08-07 11:10:03 +0200

    test shouldPreserveOrder_WhenMissionsHaveDifferentRocketsAssignedCount

--- src/test/java/org/example/MissionTests.java
+++ src/test/java/org/example/MissionTests.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -64,5 +70,32 @@ public class MissionTests {
         // then
         assertEquals(name, mission.getName());
         assertEquals(1, mission.getAssignedRocketsCount());
+    }
+
+    @Test
+    void shouldPreserveOrder_WhenMissionsHaveDifferentRocketsAssignedCount() {
+        // given
+        Mission mission1 = new Mission("aa");
+        mission1.incrementRocketsCount();
+
+        Mission mission2 = new Mission("ab");
+        mission2.incrementRocketsCount();
+        mission2.incrementRocketsCount();
+
+        Mission mission3 = new Mission("ac");
+        mission3.incrementRocketsCount();
+        mission3.incrementRocketsCount();
+        mission3.incrementRocketsCount();
+
+        List<Mission> missionList = Arrays.asList(mission2, mission1, mission3);
+
+        // when
+        Collections.sort(missionList);
+
+        // then
+        List<Mission> expectedList = Arrays.asList(mission3, mission2, mission1);
+        assertThat(missionList).hasSize(3);
+        assertThat(missionList).isSortedAccordingTo(Mission::compareTo);
+        assertThat(missionList).hasSameElementsAs(expectedList);
     }
 }
